2a0ae4a72009341767fb0703ecdaa8d7229eb7e8
djMaxM <djMaxM@d3f090b0-623f-0410-a0b0-f33c516bfeac>
2007-11-20 22:18:41 +0000

    Fixed command line problem since 1.1.1 now passes command args when running from SpringBoard

--- iPhoneMain.m
+++ iPhoneMain.m
@@ -5,12 +5,23 @@
 #import "QRCodeApplication.h"
 #import "QRCodeDecoder.h"
 
+#include <stdio.h>
+
+void debug(NSString *msg)
+{
+  NSString *s = [NSString stringWithFormat:@"%@: %@\n", [[NSDate date] description], msg];
+  FILE *fp = fopen("/tmp/QRDecode.log", "a");
+  fputs([s UTF8String], fp);
+  fflush(fp);
+  fclose(fp);
+}
+
 int main(int argc, char **argv)
 {
   NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
   int appReturn = 0;
-  
-  if (argc > 1)
+
+  if (argc > 1 && argv[1][0] != '-')
     {
       QRCodeDecoder *decoder = [[QRCodeDecoder alloc] init];
       #ifndef NOTPHONE
